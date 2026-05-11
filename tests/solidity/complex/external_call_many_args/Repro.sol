129f83e734cd2adc4fdc867d7bdefbf652e986ef
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2026-05-12 00:15:58 +0800

    test(many-args): add complex two-file repro mirroring upstream report

---
+++ tests/solidity/complex/external_call_many_args/Repro.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+contract PlaceholderContract {
+    function check_entrypoint(bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, bool, bool)
+        public
+        pure
+        returns (bool)
+    {
+        return true;
+    }
+}
