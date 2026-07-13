5c9e4484370e07827d095081b4133dc416a4c3fa
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2026-07-13 22:32:05 +0800

    style: satisfy clippy collapsible-conditional lints on Rust 1.96

--- solx-solc-test-adapter/src/test/function_call/parser/lexical/stream/integer/mod.rs
+++ solx-solc-test-adapter/src/test/function_call/parser/lexical/stream/integer/mod.rs
@@ -72,13 +72,12 @@ pub fn parse(input: &str) -> Result<Output, Error> {
             },
             State::Minus => match character {
                 Some(character) => {
-                    if Integer::CHARACTERS_DECIMAL.contains(&character) {
-                        integer.push(character);
-                        size += 1;
-                        state = State::Decimal;
-                    } else {
+                    if !Integer::CHARACTERS_DECIMAL.contains(&character) {
                         return Err(Error::NotAnInteger);
                     }
+                    integer.push(character);
+                    size += 1;
+                    state = State::Decimal;
                 }
                 None => return Err(Error::NotAnInteger),
             },
